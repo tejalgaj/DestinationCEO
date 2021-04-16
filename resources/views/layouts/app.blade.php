10323b9a4e7b7ff18e5cf3e2df85f2d59b51867c
Tejal <gajjartejal369@gmail.com>
2021-04-16 00:21:31 -0400

    footer changes

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -177,7 +177,7 @@
       <div class="footer-text" style="width: 40rem;">
             
            
-           <p> <b>Disclaimer-</b>The information contained on this site was correct at the time it was posted. Be aware that it is possible there may have been subsequent changes, 
+           <p style="line-height: 24px"> <b>Disclaimer-</b>The information contained on this site was correct at the time it was posted. Be aware that it is possible there may have been subsequent changes, 
            which make the information outdated at the time you are accessing it. </p>
            <p>Copyright © 2021 | Destination CEO | All Rights Reserved</p>
           </div>
